415740c4b836e17309bc84156e1b64cdbf75ad03
Austin Wong <austinow@gmail.com>
2018-06-29 15:34:29 -0700

    bootstrap done

---
+++ public/src/app/new/new.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { TasksService } from '../tasks.service';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-new',
+  templateUrl: './new.component.html',
+  styleUrls: ['./new.component.css']
+})
+export class NewComponent implements OnInit {
+  newPet: any;
+  msg: any;
+  exist: any;
+  foods: any;
+ 
+
+  constructor(
+    private _taskService: TasksService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) { }
+
+  ngOnInit() {
+    this.msg = false;
+    this.showDish();
+  }
+
+  logout(){
+    this._taskService.logout().subscribe(res => {
+      if(res['Login'] == false){
+        this._router.navigate(['/']);
+      }
+    })
+  }
+
+  addNew(newDish:NgForm){
+    console.log(newDish.value)
+    this._taskService.addDish(newDish.value).subscribe(data => {
+      if (data['Status'] == true){
+        console.log(data['Status'])
+        console.log("successfully added", data)
+        newDish.reset();
+        this.showDish();
+      } else {
+        console.log(data)
+        this.msg = data['Error']
+      }
+    })
+  }
+
+  showDish(){
+    this._taskService.showDish().subscribe(data => {
+      this.foods = data['food']
+      console.log("success", data)
+    })
+  }
+
+
+delete(id){
+  this._taskService.deleteDish(id).subscribe(data => {
+    console.log("deleted dish", data)
+    this.showDish()
+  })
+}
+}
---
+++ server.js
@@ -0,0 +1,42 @@
+
+
+var express = require('express');
+var app = express();
+var path = require('path');
+const session = require('express-session');
+
+
+
+
+app.use(session({
+    secret: 'keyboardkitteh',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+  }))
+
+var bodyParser = require('body-parser')
+
+
+
+
+app.use(bodyParser.json())
+app.use(express.static( __dirname + '/public/dist/public' ));
+
+require('./server/config/mongoose.js')
+require('./server/config/routes_config.js')(app);
+
+app.all("*", (req,res,next) => {
+    res.sendFile(path.resolve("./public/dist/public/index.html"))
+  });
+const server = app.listen(8000, function(){
+    console.log("listening on port 8000")
+      });
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+  socket.emit('hello', {
+    greeting: 'hello Aman'
+  })
+
+})
+
